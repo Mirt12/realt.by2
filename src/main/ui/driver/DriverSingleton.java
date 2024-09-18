bba1a82ba56ffd910934e3120cdcd1d1bd45bfac
Mirt12 <Mirt12@yandex.ru>
2024-09-18 23:32:44 +0200

    add getDriver(), constructor, quit() to DriverSingleton class

--- src/main/ui/driver/DriverSingleton.java
+++ src/main/ui/driver/DriverSingleton.java
@@ -1,8 +1,32 @@
 package main.ui.driver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class DriverSingleton {
     private static WebDriver driver;
 
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-cache", "--incognito", "--remote-allow-origins=*");
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        }
+        return driver;
+    }
+
+    public DriverSingleton() {
+    }
+
+    public static void quit() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
--- src/main/ui/page/HomePage.java
+++ src/main/ui/page/HomePage.java
@@ -1,5 +1,19 @@
 package main.ui.page;
 
+import main.ui.driver.DriverSingleton;
+import org.openqa.selenium.WebDriver;
+
 public class HomePage {
+    private WebDriver driver;
     private String baseUrl = "https://realt.by/";
+
+    public HomePage() {
+        this.driver = DriverSingleton.getDriver();
+    }
+
+    public HomePage getBaseURL() {
+        driver.get(baseUrl);
+        return this;
+    }
+
 }
