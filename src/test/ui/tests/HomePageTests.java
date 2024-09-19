957dba4471eb8f6917865acddb3ea841c4803c3c
Mirt12 <Mirt12@yandex.ru>
2024-09-19 22:31:46 +0200

    refactored openBrowser()

--- src/test/ui/tests/HomePageTests.java
+++ src/test/ui/tests/HomePageTests.java
@@ -20,9 +20,14 @@ public class HomePageTests {
         driver.manage().window().maximize();
         driver.get("https://realt.by");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
-        String loginBtnLocator = "//nav/a[@href='/login/?nextPage=/']";
-        WebElement loginBtn = driver.findElement(By.xpath(loginBtnLocator));
-        loginBtn.click();
+        String loginBtnLocator = "//*[@id=\"mainHeader\"]/div/div/div[2]/div/a";
+        driver.findElement(By.xpath(loginBtnLocator)).click();
+        String loginInputFieldLocator = "//input[@name='login']";
+        driver.findElement(By.xpath(loginInputFieldLocator)).sendKeys("Galija");
+        String submitBtnLocator = "//button[@type='submit']";
+        driver.findElement(By.xpath(submitBtnLocator)).click();
+        String emailInputFielLocator = "//input[@id='email']";
+
     }
 
 }
