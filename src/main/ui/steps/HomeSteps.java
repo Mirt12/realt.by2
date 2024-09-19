bcbc03324b5581d5d9a5d1e5c09b5d7bebb89829
Mirt12 <Mirt12@yandex.ru>
2024-09-19 20:20:12 +0200

    add HomePageTests class, add steps for login btn

---
+++ src/main/ui/steps/HomeSteps.java
@@ -0,0 +1,5 @@
+package main.ui.steps;
+
+
+public class HomeSteps {
+}
---
+++ src/test/ui/tests/HomePageTests.java
@@ -0,0 +1,28 @@
+package test.ui.tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Wait;
+
+import java.time.Duration;
+
+public class HomePageTests {
+    @Test
+    public void openBrowser() {
+        WebDriver driver;
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-cache", "--incognito", "--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://realt.by");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        String loginBtnLocator = "//a[@href='/login/?nextPage=/']//span[@class='truncate ml-1.5 ml-2.5' and text()='Войти']";
+        WebElement loginBtn = driver.findElement(By.xpath(loginBtnLocator));
+        loginBtn.click();
+    }
+
+}
