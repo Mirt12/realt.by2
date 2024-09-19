f1900e985dee25e5c26f4fcb2755e88a68fc5365
Mirt12 <Mirt12@yandex.ru>
2024-09-19 23:08:04 +0200

    renamed openBrowser() method to loginWithIncorrectEmail() AND test is ready

--- src/test/ui/tests/HomePageTests.java
+++ src/test/ui/tests/HomePageTests.java
@@ -1,5 +1,6 @@
 package test.ui.tests;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -12,7 +13,7 @@ import java.time.Duration;
 
 public class HomePageTests {
     @Test
-    public void openBrowser() {
+    public void loginWithIncorrectEmail() {
         WebDriver driver;
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-cache", "--incognito", "--remote-allow-origins=*");
@@ -22,12 +23,21 @@ public class HomePageTests {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
         String loginBtnLocator = "//*[@id=\"mainHeader\"]/div/div/div[2]/div/a";
         driver.findElement(By.xpath(loginBtnLocator)).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
         String loginInputFieldLocator = "//input[@name='login']";
         driver.findElement(By.xpath(loginInputFieldLocator)).sendKeys("Galija");
         String submitBtnLocator = "//button[@type='submit']";
         driver.findElement(By.xpath(submitBtnLocator)).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
         String emailInputFielLocator = "//input[@id='email']";
-
+        String submitRegistrationBtnLocator = "//button[@type='submit']";
+        driver.findElement(By.xpath(emailInputFielLocator)).sendKeys("zfFr66h!_jhj1");
+        driver.findElement(By.xpath(submitRegistrationBtnLocator)).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        String expectedErrorText = "Некорректный Email";
+        String errorLocator = "//div[@class='text-danger-500 pt-1.5 text-subhead']";
+        WebElement actualError = driver.findElement(By.xpath(errorLocator));
+        String actualErrorText = actualError.getText();
+        Assertions.assertEquals(expectedErrorText, actualErrorText);
     }
-
 }
